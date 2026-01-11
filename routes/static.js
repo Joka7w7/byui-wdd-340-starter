ecd7ee9ab5aa01dbb4e6c8533affadd4096889fe
Jose Chanax <chanaxjose@gmail.com>
2026-01-11 16:36:40 -0600

    Refactor styles and update layout for homepage; add hero section with vehicle details and reviews

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const path = require("path")
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+// Absolute path to /public
+const publicPath = path.join(__dirname, "..", "public")
 
-module.exports = router;
+router.use(express.static(publicPath))
 
-
+module.exports = router
+console.log("Serving static from:", publicPath)
 
