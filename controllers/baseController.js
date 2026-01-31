89f2987186978030137a29898dc21ac2b07313aa
Jose Chanax <chanaxjose@gmail.com>
2026-01-30 19:25:05 -0600

    feat: Implement user authentication with login and registration
    - Added accountController to handle login and registration views and logic.
    - Created account-model for database interactions related to user accounts.
    - Introduced accountRoute for handling account-related routes.
    - Implemented account-validation for validating registration and login data.
    - Updated views for login and registration to include flash messages for user feedback.
    - Integrated express-session and connect-pg-simple for session management.
    - Added necessary dependencies in package.json and pnpm-lock.yaml.
    - Modified baseController to include flash messages on the home page.
    - Enhanced styles.css to accommodate new UI elements for flash messages and forms.

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  //req.flash("notice", "Welcome to CSE Motors!")
   res.render("index", {title: "Home", nav})
 }
 
--- server.js
+++ server.js
@@ -13,6 +13,34 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +57,7 @@ app.use(static)
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", inventoryRoute)
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
